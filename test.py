c9aae2c94b31624162041035fedcac451fa3c592
rekcilF <rekcilF77@gmail.com>
2024-03-22 06:38:45 +1000

    LETS GET IT LETS GET IT LETS GO

--- test.py
+++ test.py
@@ -166,20 +166,19 @@ def lister(table, noOfVar):
     x = 0
     max = -1
     count = 1
+    start = -1
     for p in range(0, noOfVar):
         thing = (((2 ** noOfVar) // (2 ** x)) // 2)
         print(thing, "thing")
-        start = (((2**x) - 1) + (p // thing))
-        startt = (2**p) - 1
-        check = p // thing
-        realStart = (startt) + check
+        print(start,"before")
+        start += 2**x
+        print(start,"after")
         max += (2**(x+1))
-        print(thing, "thing", start, "start", max, "max", count, "count", p, "p", ((2**x) - 1), "start no p")
-        print(realStart, "realstart", check, startt, 2**p, p, thing)
+
+
 
         for z in range(0, thing):
-            print(z,"z", thing)
-            for x in range(realStart,(max+1)):
+            for x in range(start,(max+1)):
                 newList.append(listTable[x])
                 print("added", listTable[x], x)
             count += 1
